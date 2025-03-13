98103681f5325eb21c464e09769911c321147509
Syfe <22247810+ItsSyfe@users.noreply.github.com>
2025-03-13 14:43:11 +0000

    build hard fork instead

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ git clone https://github.com/PaperMC/Folia
 git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
 cd Folia
+git checkout dev/hard-fork
 ./gradlew applyPatches
-./gradlew createReobfPaperclipJar
+./gradlew createMojmapBundlerJar
